7f6e7ad56b09208b6863022c598cda7b56df27f8
Sergii Horoshko <sergii.horoshko@mev.com>
2018-08-03 11:11:07 +0300

    hotFIX querie 1.8

--- queries/1.8.sql
+++ queries/1.8.sql
@@ -1,7 +1,9 @@
 USE mevblog;
 
 SELECT a.idauthor, a.first_name, a.last_name,
-COUNT(p.author_idauthor) AS postcount
+COUNT(p.author_idauthor) AS tagstop
 FROM author a
 JOIN posts p ON (a.idauthor = p.author_idauthor)
-GROUP BY a.idauthor LIMIT 5;
+GROUP BY a.idauthor 
+ORDER BY tagstop DESC
+LIMIT 5;
